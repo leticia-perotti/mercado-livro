3123d29436a46d55dc3d44bd33e475bc8b01f013
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2022-09-22 21:54:29 -0300

    Get e Post

--- src/main/kotlin/com/mercadolivro/mercadolivro/controller/CustumerController.kt
+++ src/main/kotlin/com/mercadolivro/mercadolivro/controller/CustumerController.kt
@@ -1,30 +1,68 @@
 package com.mercadolivro.mercadolivro.controller
 
 import com.mercadolivro.mercadolivro.controller.request.PostCostumerRequest
+import com.mercadolivro.mercadolivro.controller.request.PutCostumerRequest
 import com.mercadolivro.mercadolivro.model.CostumerModel
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("customer")
 class CustumerController {
 
+    val costumers = mutableListOf<CostumerModel>()
+
     @GetMapping
-    fun getCostumer(): CostumerModel {
-        return CostumerModel(
-            "1", "Maria", "marios@armario"
-        )
+    fun getAll(): List<CostumerModel> {
+        return costumers
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun crateCostumer(
         @RequestBody custumer: PostCostumerRequest
     ) {
-        println(custumer)
+        val id = if(costumers.isEmpty()) 1 else costumers.last().id.toInt() + 1
+        costumers.add(CostumerModel(
+            id = id.toString(),
+            name = custumer.name,
+            email = custumer.email
+        ))
     }
 
+    @GetMapping("/{id}")
+    fun getCostumer(
+        @PathVariable id: String
+    ): CostumerModel{
+        return costumers.filter { it.id == id }.first()
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun putCostumer(
+        @PathVariable id: String,
+        @RequestBody costumer: PutCostumerRequest
+    ){
+        costumers.filter { it.id == id }.first().let {
+            it.name  = costumer.name
+            it.email = costumer.email
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deleteCostumer(
+        @PathVariable id: String
+    ){
+        costumers.removeIf{it.id == id}
+    }
 
 }
