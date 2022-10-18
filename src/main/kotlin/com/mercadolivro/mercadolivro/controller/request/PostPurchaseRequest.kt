ce2bcef7c2b1fc0b20db442eec0d3f3f59351639
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2022-10-17 22:55:50 -0300

    migracao banco

---
+++ src/main/kotlin/com/mercadolivro/mercadolivro/controller/request/PostPurchaseRequest.kt
@@ -0,0 +1,14 @@
+package com.mercadolivro.mercadolivro.controller.request
+
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Positive
+
+data class PostPurchaseRequest(
+
+    @field:NotNull
+    @field:Positive
+    val costumerId: Int,
+
+    @field:NotNull
+    val bookId: Set<Int>
+)
---
+++ src/main/resources/db/migration/V4__create_table_purchase_and_purchase_book.sql
@@ -0,0 +1,16 @@
+create table purchase(
+    id int auto_increment primary key,
+    costumer_id int not null,
+    nfe varchar(255),
+    price decimal(15,2) not null,
+    created_at DATETIME not null,
+    foreign key (costumer_id) references costumer(id)
+);
+create table purchase_book(
+    purchase_id int not null,
+    book_id int not null,
+    FOREIGN KEY (purchase_id) references purchase(id),
+    FOREIGN KEY (book_id) references book(id),
+
+    PRIMARY key (purchase_id, book_id)
+);
