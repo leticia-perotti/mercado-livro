36ed851e4cebfcecac56e1a05f88bbe41575bf8f
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2022-11-01 22:12:36 -0300

    Bloquei de rota e permissão de especificas

--- src/main/kotlin/com/mercadolivro/mercadolivro/config/SecurityConfig.kt
+++ src/main/kotlin/com/mercadolivro/mercadolivro/config/SecurityConfig.kt
@@ -2,11 +2,29 @@ package com.mercadolivro.mercadolivro.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @Configuration
+@EnableWebSecurity
+class SecurityConfig: WebSecurityConfigurerAdapter() {
 
-class SecurityConfig {
+    private val publicPostMatchers = arrayOf(
+        "/customer"
+    )
+
+    override fun configure(http: HttpSecurity) {
+        http.cors().and().csrf().disable()
+        http.authorizeHttpRequests()
+            .antMatchers(HttpMethod.POST, *publicPostMatchers).permitAll()
+            .anyRequest().authenticated()
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    }
+
     @Bean
     fun BCryptPasswordEncoder(): BCryptPasswordEncoder{
         return org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder()
