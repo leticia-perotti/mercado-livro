751c361e31132777e6bced21734b7bf1d3d4d51f
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2022-11-12 20:45:22 -0300

    Teste unitário

---
+++ src/test/kotlin/com/mercadolivro/mercadolivro/service/CostumerServiceTest.kt
@@ -0,0 +1,60 @@
+package com.mercadolivro.service
+
+import com.mercadolivro.mercadolivro.enum.CostumerStatus
+import com.mercadolivro.mercadolivro.enum.Role
+import com.mercadolivro.mercadolivro.model.CostumerModel
+import com.mercadolivro.mercadolivro.repository.CostumerRepository
+import com.mercadolivro.mercadolivro.service.BookService
+import com.mercadolivro.mercadolivro.service.CostumerService
+import io.mockk.*
+import io.mockk.impl.annotations.*
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import java.util.*
+
+@ExtendWith(MockKExtension::class)
+class CostumerServiceTest {
+    @MockK
+    private lateinit var customerRepository: CostumerRepository
+
+    @MockK
+    private lateinit var bookService: BookService
+
+    @MockK
+    private lateinit var bCrypt: BCryptPasswordEncoder
+
+    @InjectMockKs
+    private lateinit var customerService: CostumerService
+
+    @Test
+    fun `should return all customers`() {
+        val fakeCustomers = listOf(buildCustomer(), buildCustomer())
+
+        every { customerRepository.findAll() } returns fakeCustomers
+
+        val customers = customerService.getAll(null)
+
+        assertEquals(fakeCustomers, customers)
+        verify(exactly = 1) { customerRepository.findAll() }
+        verify(exactly = 0) { customerRepository.findByNameContaining(any()) }
+    }
+
+    fun buildCustomer(
+        id: Int? = null,
+        name: String = "customer name",
+        email: String = "${UUID.randomUUID()}@email.com",
+        password: String = "password"
+    ) = CostumerModel(
+        id = id,
+        name = name,
+        email = email,
+        status = CostumerStatus.ATIVO,
+        password = password,
+        roles = setOf(Role.COSTUMER)
+    )
+
+}
